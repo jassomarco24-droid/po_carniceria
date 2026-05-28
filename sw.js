357c3efe0a5088e2b15550b30c0aa9158f4f486a
jassomarco24-droid <jassomarco24@gmail.com>
2026-05-27 23:32:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,30 @@
-const CACHE_NAME = '7toros-cache-v85';
+const CACHE_NAME = '7toros-cache-v90';
 const ASSETS = [
   './',
   './index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
-// 1. Fase de Instalacion: Almacena archivos estaticos esenciales y fuerza activacion inmediata
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting(); // Corta de golpe la sala de espera del navegador
+  self.skipWaiting(); 
 });
 
-// 2. Fase de Activacion: Purga y elimina de forma absoluta cualquier cache viejo en el almacenamiento interno
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.map(key => {
         if (key !== CACHE_NAME) {
-          return caches.delete(key); // Destruye versiones de cache obsoletas
+          return caches.delete(key); 
         }
       })
     ))
   );
-  self.clients.claim(); // Toma el control total de la pestaña del cliente inmediatamente
+  self.clients.claim(); 
 });
 
-// 3. Estrategia de Red: Intenta obtener datos de internet en primer lugar, si falla, sirve del búnker offline
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
